54e65ac1dbf411b81d5dcb279bf3199c6eea6747
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-12 19:28:12 -0600

    starting js portion

---
+++ script.js
@@ -0,0 +1,26 @@
+const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+// NASA API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+// Get 10 images from NASA API
+async function getNasaPictures() {
+    try {
+      const response = await fetch(apiUrl);
+      resultsArray = await response.json();
+      console.log(resultsArray);
+    } catch (error) {
+    // Catch Error here
+    }
+}
+
+// On Load
+getNasaPictures();
