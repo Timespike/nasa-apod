da4984b477a81af621a76b54e8657ee17fcc411a
Timespike <50344633+Timespike@users.noreply.github.com>
2022-01-12 19:42:24 -0600

    added DOM update function

--- script.js
+++ script.js
@@ -11,12 +11,55 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 
 let resultsArray = [];
 
+function updateDOM() {
+    resultsArray.forEach((result) => {
+        // Card Container
+        const card = document.createElement('div');
+        card.classList.add('card');
+        // Link 
+        const link = document.createElement('a');
+        link.href = result.hdurl;
+        link.title = 'View Full Image';
+        link.target = '_blank';
+        // Image
+        const image = document.createElement('img');
+        image.src = result.url;
+        image.alt = 'NASA Picture of the Day';
+        image.loading = 'lazy';
+        image.classList.add('card-img-top');
+        // Card Body
+        const cardBody = documen.createElement('div');
+        cardBody.classList.add('card-body');
+        // Card Title
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = result.title;
+        // Save Text
+        const saveText = document.createElement('p');
+        saveText.classList.add('clickable');
+        saveText.textContent = 'Add To Favorites';
+        // Card Text
+        const cardText = document.createElement('p');
+        cardText.textContent = result.explanation;
+        //  Footer Container
+        const footer = document.createElement('small');
+        footer.classList.add('text-muted');
+        // Date
+        const date = document.createElement('strong');
+        date.textContent = result.date;
+        // Copyright
+        const copyright = document.createElement('span');
+        copyright.textContent = ` ${result.copyright}`;
+    });
+}
+
 // Get 10 images from NASA API
 async function getNasaPictures() {
     try {
       const response = await fetch(apiUrl);
       resultsArray = await response.json();
       console.log(resultsArray);
+      updateDOM();
     } catch (error) {
     // Catch Error here
     }
